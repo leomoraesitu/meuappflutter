662c27701b914a5916c551fd7a70bf7953a38626
leomoraesitu <leomoraesitu@hotmail.com>
2024-09-18 14:46:12 -0300

    pre-fix

--- lib/my_flutter_app_icons.dart
+++ lib/my_flutter_app_icons.dart
@@ -17,6 +17,7 @@
 ///         License:   SIL (http://scripts.sil.org/OFL)
 ///         Homepage:  http://nagoshiashumari.github.io/Rpg-Awesome/
 ///
+library;
 import 'package:flutter/widgets.dart';
 
 class MyFlutterApp {
